8a82212ad85222b095fd9262187f20c05b606f59
bluishflame <jyb0422@naver.com>
2023-07-19 16:52:12 +0900

    20230719_01

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import './MainPage.css';
+import styled from 'styled-components';
 
 const MainPage = () => {
   return (
-    <div className="main-container">
-      <div className="content">MainPage</div>
-    </div>
+    <MainContainer>
+      <Content>MainPage</Content>
+    </MainContainer>
   );
 }
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: red;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 844px;
+  background-color: white;
+`;
 
 export default MainPage;
