d2cc7fa75818c2ba1631e31822f27cd54f522ab3
bluishflame <jyb0422@naver.com>
2023-07-20 18:58:55 +0900

    Design: 메뉴바, 로고 구현
    메뉴바와 로고를 대략적으로 구현함. 클릭해서 다른 곳으로 넘어가지는 않음. 아이콘 이미지 대신 텍스트로만 작성

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,7 +5,13 @@ const MainPage = () => {
   return (
     <PageWrapper>
       <RedScreen>
+        <LogoText>LOGO</LogoText> 
         MainPage
+        <Menu>
+          <MenuButton>HOME</MenuButton>
+          <MenuButton>MAKE CARD</MenuButton>
+          <MenuButton>MY PAGE</MenuButton>
+        </Menu>
       </RedScreen>
     </PageWrapper>
   );
@@ -21,14 +27,43 @@ const PageWrapper = styled.div`
 `;
 
 const RedScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 390px;
   height: 844px;
   background-color: red;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
+  position: relative; 
+`;
+
+const LogoText = styled.div`
+  position: absolute;
+  top: 47px; 
+  left: 18px; 
   color: black;
+  font-size: 20px; 
+  font-weight: bold;
+`;
+
+const Menu = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: blue;
+  position: absolute; 
+  bottom: 0; 
+`;
+
+const MenuButton = styled.button`
+  width: 120px;
+  height: 50px;
+  background-color: blue;
+  color: white;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
 `;
 
 export default MainPage;
