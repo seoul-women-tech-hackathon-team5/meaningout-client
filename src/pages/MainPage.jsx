3b084c64b5a1b10b682c25ca42279d6b94970d1d
bluishflame <jyb0422@naver.com>
2023-07-21 13:47:56 +0900

    Feat: 무한 스크롤 기능 추가
    무한 스크롤 기능을 추가했음 문제는 빨간 화면 밖으로 무한스크롤이 되고 있다는 점. 수정해서 다시 업로드할 예정.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const MainPage = () => {
+  const [cards, setCards] = useState(generateRandomCards(10));
+
+  const fetchMoreData = () => {
+    setCards((prevCards) => [...prevCards, ...generateRandomCards(10)]);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      fetchMoreData();
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <PageWrapper>
       <RedScreen>
-        <LogoText>LOGO</LogoText> 
+        <LogoText>LOGO</LogoText>
         MainPage
         <Menu>
           <MenuButton>HOME</MenuButton>
           <MenuButton>MAKE CARD</MenuButton>
           <MenuButton>MY PAGE</MenuButton>
         </Menu>
+        <InfiniteScroll
+          dataLength={cards.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          style={{ overflow: 'hidden' }}
+        >
+          {cards.map((card, index) => (
+            <Card key={index} style={{ backgroundColor: card.color }} />
+          ))}
+        </InfiniteScroll>
       </RedScreen>
     </PageWrapper>
   );
-}
+};
+
+const generateRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const generateRandomCards = (count) => {
+  const cards = [];
+  for (let i = 0; i < count; i++) {
+    cards.push({ color: generateRandomColor() });
+  }
+  return cards;
+};
+
+const Card = styled.div`
+  width: 350px;
+  height: 530px;
+  margin: 8px;
+`;
 
 const PageWrapper = styled.div`
   display: flex;
@@ -31,17 +75,17 @@ const RedScreen = styled.div`
   flex-direction: column;
   align-items: center;
   width: 390px;
-  height: 844px;
+  height: 100vh;
   background-color: red;
-  position: relative; 
+  position: relative;
 `;
 
 const LogoText = styled.div`
   position: absolute;
-  top: 47px; 
-  left: 18px; 
+  top: 47px;
+  left: 18px;
   color: black;
-  font-size: 20px; 
+  font-size: 20px;
   font-weight: bold;
 `;
 
@@ -52,8 +96,8 @@ const Menu = styled.div`
   justify-content: space-between;
   align-items: center;
   background-color: blue;
-  position: absolute; 
-  bottom: 0; 
+  position: absolute;
+  bottom: 0;
 `;
 
 const MenuButton = styled.button`
