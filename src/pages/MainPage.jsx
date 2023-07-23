8c006bc771b10916660a378068eeb8dbe2826a58
bluishflame <jyb0422@naver.com>
2023-07-23 20:26:24 +0900

    Design: 메뉴 및 로고 고정 완료
    무한스크롤 화면 완성/메뉴 및 로고 고정했으며 무한스크롤도 정상 작동하고 있음 무한스크롤바도 안 보이게 했음

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -30,24 +30,26 @@ const MainPage = () => {
   return (
     <PageWrapper>
       <RedScreen ref={redScreenRef}>
-        <LogoText>LOGO</LogoText>
-        MainPage
-        <Menu>
-          <MenuButton>HOME</MenuButton>
-          <MenuButton>MAKE CARD</MenuButton>
-          <MenuButton>MY PAGE</MenuButton>
-        </Menu>
+        <LogoContainer>
+          <LogoText>LOGO</LogoText>
+        </LogoContainer>
         <InfiniteScroll
           dataLength={cards.length}
           next={fetchMoreData}
           hasMore={true}
           loader={<h4>Loading...</h4>}
-          style={{ overflow: 'hidden', marginTop: '70px' }}
         >
-          {cards.map((card, index) => (
-            <Card key={index} style={{ backgroundColor: card.color }} />
-          ))}
+          <CardContainer>
+            {cards.map((card, index) => (
+              <Card key={index} style={{ backgroundColor: card.color }} />
+            ))}
+          </CardContainer>
         </InfiniteScroll>
+        <Menu>
+          <MenuButton>HOME</MenuButton>
+          <MenuButton>MAKE CARD</MenuButton>
+          <MenuButton>MY PAGE</MenuButton>
+        </Menu>
       </RedScreen>
     </PageWrapper>
   );
@@ -90,36 +92,59 @@ const RedScreen = styled.div`
   background-color: red;
   position: relative;
   overflow-y: scroll;
+
+  /* Hide the scrollbar */
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const LogoContainer = styled.div`
+  width: 100%;
+  position: sticky;
+  top: 0;
+  background-color: red;
+  z-index: 1;
 `;
 
 const LogoText = styled.div`
-  position: absolute;
-  top: 47px;
-  left: 18px;
   color: black;
   font-size: 20px;
   font-weight: bold;
+  padding: 20px;
 `;
 
 const Menu = styled.div`
-  width: 100%;
-  height: 80px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   background-color: blue;
-  position: absolute;
+  width: 100%;
+  height: 80px;
+  padding: 0 20px;
+  position: sticky;
   bottom: 0;
+  z-index: 1;
+  overflow-x: hidden;
+  overflow-y: auto;
 `;
 
 const MenuButton = styled.button`
-  width: 120px;
+  flex: 1;
   height: 50px;
   background-color: blue;
   color: white;
   border: none;
   font-size: 16px;
   cursor: pointer;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 export default MainPage;
