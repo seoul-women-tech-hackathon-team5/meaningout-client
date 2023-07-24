f6d1402610a8785fcee6bdcc283d6d2e70a06bd1
anonymousrecords <a.Records2022@gmail.com>
2023-07-25 06:37:16 +0900

    feat : Implement MainPage

---
+++ src/components/MainPage/AreaItem.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const StyledAreaItem = styled.div`
+  font-size: 24px;
+  font-weight: 800;
+
+  button {
+    display: flex;
+    align-items: center;
+  }
+
+  .toggle-container {
+    width: 100px;
+    height: 220px;
+    padding: 12px;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 400;
+    color: #A9A9A9;
+    background-color: white;
+    cursor: pointer;
+    border: solid 1px #D9D9D9;
+  }
+
+  .toggle-container > div:hover {
+    color: black;
+    font-weight: 600;
+  }
+`;
+
+export default function AreaItem({ title, content, onContentClick }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const handleContentClick = (clickedTitle) => {
+    onContentClick(clickedTitle);
+    setIsExpanded(false);
+  };
+
+  return (
+    <StyledAreaItem>
+      <button onClick={handleToggle}>
+        {isExpanded ? title : title}
+        <img src='/assets/icons/toggle.svg' alt='toggle' />
+      </button>
+      {isExpanded && (
+        <div className='toggle-container'>
+          {content.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleContentClick(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
+    </StyledAreaItem>
+  );
+}
+
+AreaItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onContentClick: PropTypes.func.isRequired,
+};
---
+++ src/components/UI/BottomNav.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const StyledBottomNav = styled.div`
+    height: 5.188rem;
+    position: fixed;
+    bottom: 0;
+
+    display: flex;
+    justify-content: center;
+    border-top-style: solid;
+    border-top-width: 0.5px;
+
+    button { 
+        width: 8.125rem;
+        height: 2.875rem;
+        padding: 8.58px 0 6px 0;
+    }
+
+    img {
+        width: 1.5rem;
+        height: 1.5rem;
+    }
+
+    .nav-item {
+        color: grey;
+    }
+
+    .active {
+        color: #2294FF;
+    }
+`
+
+export default function BottomNav() {
+    const [activeNav, setActiveNav] = useState(1);
+    return (
+        <StyledBottomNav>
+            <nav>
+                <Link to="/main" onClick={() => setActiveNav(1)}>
+                    <button
+                        className={activeNav === 1 ? "nav-item active" : "nav-item"}
+                    >
+                        <img src='/assets/icons/home.svg' alt='people'/>
+                        <div>홈</div>
+                    </button>
+                </Link>
+                <Link to="/certificate" onClick={() => setActiveNav(2)}>
+                    <button
+                        className={activeNav === 2 ? "nav-item active" : "nav-item"}
+                    >
+                        <img src='/assets/icons/make.svg' alt='people'/>
+                        <div>make</div>
+                    </button>
+                </Link>
+                <Link to="/my" onClick={() => setActiveNav(3)}>
+                    <button
+                        className={activeNav === 3 ? "nav-item active" : "nav-item"}
+                    >
+                        <img src='/assets/icons/mypage.svg' alt='people'/>
+                        <div>mypage</div>
+                    </button>
+                </Link>
+            </nav>
+        </StyledBottomNav>
+    );
+}
