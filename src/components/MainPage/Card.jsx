0398a61bcc10a28f97436da4736f1de9606085c1
bluishflame <jyb0422@naver.com>
2023-07-28 16:05:24 +0900

    Feat: Card 기능 작업중
    작업중이던 거 branch 다시 파기 위해서 일단 백업

--- src/components/MainPage/Card.jsx
+++ src/components/MainPage/Card.jsx
@@ -1,4 +1,30 @@
-import { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
+const CardContainer = styled.div`
+  display: flex;
+`;
+
+const StyledCard = styled.div`
+  width: 150px;
+  height: 200px;
+  background-color: #558BCF;
+  margin: 8px;
+  border-radius: 8px;
+
+  display: flex;
+  padding: 20px;
+`;
+
+const Card = () => {
+  // Add any necessary logic here if required for the cards
+
+  return (
+    <CardContainer>
+      <StyledCard />
+      <StyledCard />
+    </CardContainer>
+  );
+};
+
+export default Card;
